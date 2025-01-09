b4be8f7cbf373463d25eebdcd8cb07de9e4e5e1b
aditi644 <120701036+aditi644@users.noreply.github.com>
2025-01-09 14:27:10 +0530

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ import dotenv from "dotenv";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); //to parse form data(urlenoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
