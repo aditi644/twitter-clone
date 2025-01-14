546a5987f97e54325a7c5e2a733b5c055dbb527c
aditi644 <120701036+aditi644@users.noreply.github.com>
2025-01-14 17:28:36 +0530

    3- auth handled and routes protected in client

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import userRoutes from './routes/user.routes.js';
 import postRoutes from './routes/post.routes.js';
+import notificationRoutes from "./routes/notification.route.js";
 
 dotenv.config();
 
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
