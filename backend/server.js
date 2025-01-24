49a21e92b9946dc4f389d9bb3035641222549a8f
aditi644 <120701036+aditi644@users.noreply.github.com>
2025-01-24 22:19:53 +0530

    complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import {v2 as cloudinary} from "cloudinary";
+import path from "path";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended: true})); //to parse form data(urlenoded)
@@ -29,6 +31,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
